dd114e040ebe2053c660f09f73cbc3178e56d600
Vishal Agrhari <vishalagrahari2024@gmail.com>
2026-06-17 12:39:05 +0530

    model triner completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from src.components.data_transformation import DataTransformation
 
 
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -51,4 +54,8 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_tranformation = DataTransformation()
-    data_tranformation.initiate_data_tranformation(train_data,test_data)
+    data_transformation=DataTransformation()
+    train_arr, test_arr, _= data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modelTrainer = ModelTrainer()
+    print(initiate_model_trainer(train_arr,test_arr))
